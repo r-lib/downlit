7d2c175ff86164c81163312b17fd2f0c1b481c5d
Hadley Wickham <h.wickham@gmail.com>
2020-06-02 14:00:27 -0500

    Ignore line endings in test output

--- tests/testthat/test-downlit-html.R
+++ tests/testthat/test-downlit-html.R
@@ -1,6 +1,6 @@
 test_that("can highlight html file", {
   verify_output(test_path("test-downlit-html.txt"), {
     out <- downlit_html_path(test_path("autolink.html"), tempfile())
-    cat(brio::read_file(out))
+    cat(brio::read_lines(out), sep = "\n")
   })
 })
