54224b9130f503d6dbc7bc411baf6aa3c513befb
Hadley Wickham <h.wickham@gmail.com>
2021-05-04 13:03:40 -0500

    Convert verify_output to snapshot tests

--- tests/testthat/_snaps/evaluate.md
+++ tests/testthat/_snaps/evaluate.md
@@ -1,3 +1,49 @@
+# handles parsing failures gracefully
+
+    Code
+      cat(evaluate_and_highlight("1 + \n"))
+    Output
+      <div class='input'>NA</div><div class='co'>#&gt; <span class='error'>Error: </span>&lt;text&gt;:3:0: unexpected end of input#&gt; 1: 1 + #&gt; 2: #&gt;   ^</div>
+
+# handles basic cases
+
+    Code
+      cat(evaluate_and_highlight("# comment \n"))
+    Output
+      <div class='input'><span class='co'># comment </span></div>
+    Code
+      cat(evaluate_and_highlight("message('x') \n"))
+    Output
+      <div class='input'><span class='fu'><a href='https://rdrr.io/r/base/message.html'>message</a></span><span class='op'>(</span><span class='st'>'x'</span><span class='op'>)</span> </div>
+      <div class='co'>#&gt; x</div>
+    Code
+      cat(evaluate_and_highlight("warning('x') \n"))
+    Output
+      <div class='input'><span class='kw'><a href='https://rdrr.io/r/base/warning.html'>warning</a></span><span class='op'>(</span><span class='st'>'x'</span><span class='op'>)</span> </div>
+      <div class='co'>#&gt; <span class='warning'>Warning: </span>x</div>
+    Code
+      cat(evaluate_and_highlight("stop('x', call. = FALSE) \n"))
+    Output
+      <div class='input'><span class='kw'><a href='https://rdrr.io/r/base/stop.html'>stop</a></span><span class='op'>(</span><span class='st'>'x'</span>, call. <span class='op'>=</span> <span class='cn'>FALSE</span><span class='op'>)</span> </div>
+      <div class='co'>#&gt; <span class='error'>Error: </span>x</div>
+
+# combines plots as needed
+
+    Code
+      f1 <- (function() plot(1))
+      f2 <- (function() lines(0:2, 0:2))
+      cat(evaluate_and_highlight("f1()\nf2()\n", fig_save = fig_save, env = environment()))
+    Output
+      <div class='input'><span class='fu'>f1</span><span class='op'>(</span><span class='op'>)</span></div>
+      <div class='input'><span class='fu'>f2</span><span class='op'>(</span><span class='op'>)</span></div>
+      <div class='img'><img src='1.png' alt='' width='10' height='10' /></div>
+
+# handles other plots
+
+    [1] "<div class='input'><span class='fu'>f3</span><span class='op'>(</span><span class='op'>)</span></div>\n<div class='input'><span class='fu'>f4</span><span class='op'>(</span><span class='op'>)</span></div>Text for plot  4"
+    attr(,"dependencies")
+    list()
+
 # custom infix operators are linked, but regular are not
 
     <span class='nv'>x</span> <span class='o'><a href='https://rdrr.io/r/base/match.html'>%in%</a></span> <span class='nv'>y</span>
--- tests/testthat/test-evaluate.R
+++ tests/testthat/test-evaluate.R
@@ -1,45 +1,53 @@
-test_that("evaluate_and_highlight works", {
-  verify_output(test_path("test-evaluate.txt"), {
-    "Parsing failure"
-    cat(evaluate_and_highlight("1 + \n"))
+test_that("handles parsing failures gracefully", {
+  expect_snapshot(cat(evaluate_and_highlight("1 + \n")))
+})
 
-    "Basic ouput"
+test_that("handles basic cases", {
+  expect_snapshot({
     cat(evaluate_and_highlight("# comment \n"))
     cat(evaluate_and_highlight("message('x') \n"))
     cat(evaluate_and_highlight("warning('x') \n"))
     cat(evaluate_and_highlight("stop('x', call. = FALSE) \n"))
+  })
+})
 
-    "Plots"
-    fig_save <- function(plot, id) list(path = paste0(id, ".png"), width = 10, height = 10)
+test_that("combines plots as needed", {
+  fig_save <- function(plot, id) list(path = paste0(id, ".png"), width = 10, height = 10)
+  expect_snapshot({
     f1 <- function() plot(1)
     f2 <- function() lines(0:2, 0:2)
-    cat(evaluate_and_highlight("f1()\nf2()\n", fig_save = fig_save, env = environment()))
-
-    "Other plots"
-    f3 <- function()
-      structure(3, class = c("fakePlot", "otherRecordedplot"))
-    f4 <- function()
-      structure(4, class = c("fakePlot", "otherRecordedplot"))
-    # Check that we can drop the inclusion of the first one
-    is_low_change.fakePlot <- function(p1, p2) TRUE
-    print.fakePlot <- function(x, ...) {
-      x
-    }
-    replay_html.fakePlot <- function(x, ...) {
-      paste("Text for plot ", unclass(x))
-    }
-    registerS3method("is_low_change", "fakePlot",
-                     is_low_change.fakePlot,
-                     envir = asNamespace("downlit"))
-    registerS3method("replay_html", "fakePlot",
-                     replay_html.fakePlot,
-                     envir = asNamespace("downlit"))
-    registerS3method("print", "fakePlot",
-                     print.fakePlot)
-    cat(evaluate_and_highlight("f3()\nf4()", env = environment(),
-                               fig_save = fig_save,
-                               output_handler = evaluate::new_output_handler(value = print)))
+      cat(evaluate_and_highlight("f1()\nf2()\n", fig_save = fig_save, env = environment()))
   })
+})
+
+test_that("handles other plots", {
+  f3 <- function() structure(3, class = c("fakePlot", "otherRecordedplot"))
+  f4 <- function() structure(4, class = c("fakePlot", "otherRecordedplot"))
+  # Check that we can drop the inclusion of the first one
+  is_low_change.fakePlot <- function(p1, p2) TRUE
+  print.fakePlot <- function(x, ...) {
+    x
+  }
+  replay_html.fakePlot <- function(x, ...) {
+    paste("Text for plot ", unclass(x))
+  }
+  registerS3method("is_low_change", "fakePlot",
+    is_low_change.fakePlot,
+    envir = asNamespace("downlit")
+  )
+  registerS3method("replay_html", "fakePlot",
+    replay_html.fakePlot,
+    envir = asNamespace("downlit")
+  )
+  registerS3method(
+    "print", "fakePlot",
+    print.fakePlot
+  )
+  expect_snapshot_output(
+    evaluate_and_highlight("f3()\nf4()",
+    env = environment(),
+    output_handler = evaluate::new_output_handler(value = print)
+  ))
 })
 
 test_that("custom infix operators are linked, but regular are not", {
