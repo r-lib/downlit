509873bed4cd35f06304b3ad1fe3706289d71043
Adam <arawles@gfsc.gg>
2021-08-19 11:19:36 +0100

    Added a test case to check you can get the URL for an uninstalled package (we're using BMRSr but this could change to a more robust way of looking for a package with a URL)

--- tests/testthat/test-metadata.R
+++ tests/testthat/test-metadata.R
@@ -7,6 +7,16 @@ test_that("can extract urls for package", {
   expect_equal(package_urls("MASS"), "http://www.stats.ox.ac.uk/pub/MASS4/")
 })
 
+test_that("can extract urls for uninstalled packages from CRAN", {
+  skip_on_cran()
+  skip_if(requireNamespace("BMRSr", quietly = TRUE))
+
+  # We're testing here that we can find URLs for packages that aren't installed
+  # I'm assuming that BMRSr isn't going to be installed (because why would it),
+  # but this might not be the best approach
+  expect_equal(package_urls("BMRSr"), "https://bmrsr.arawles.co.uk/")
+})
+
 test_that("handle common url formats", {
   ab <- c("https://a.com", "https://b.com")
 
