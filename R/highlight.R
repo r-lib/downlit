8434f36734d1dfca434cbe86ba1facb9da99df92
dmurdoch <murdoch.duncan@gmail.com>
2022-06-09 19:06:19 -0400

    Fixes truncation of long tokens by using `utils::getParseText()` (#136)
    Fixes #128
    
    Co-authored-by: Hadley Wickham <h.wickham@gmail.com>

--- R/highlight.R
+++ R/highlight.R
@@ -121,6 +121,23 @@ line_col <- function(x) {
   data.frame(line, col)
 }
 
+# utils::getParseData will truncate very long strings or tokens;
+# this function checks for that and uses the slow
+# utils::getParseText function when necessary.
+
+getFullParseData <- function(x) {
+  res <- utils::getParseData(x)
+
+  truncated <- res$terminal &
+    substr(res$text, 1, 1) == "[" &
+    nchar(res$text) > 5  # 5 is arbitrary, 2 would probably be enough
+
+  if (any(truncated))
+    res$text[truncated] <- utils::getParseText(res, res$id[truncated])
+
+  res
+}
+
 parse_data <- function(text) {
   text <- standardise_text(text)
   stopifnot(is.character(text), length(text) == 1)
@@ -130,7 +147,7 @@ parse_data <- function(text) {
     return(NULL)
   }
 
-  list(text = text, expr = expr, data = utils::getParseData(expr))
+  list(text = text, expr = expr, data = getFullParseData(expr))
 }
 
 # Highlighting ------------------------------------------------------------
