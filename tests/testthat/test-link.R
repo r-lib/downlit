211f5f1508ca5440144cdb5582a74f7ae53830b8
Sergio Oller <sergioller@gmail.com>
2022-06-23 12:21:37 +0200

    Revert test changes since they don't require network connection anymore

--- tests/testthat/test-link.R
+++ tests/testthat/test-link.R
@@ -162,13 +162,11 @@ test_that("can link to local articles", {
 })
 
 test_that("can link to bioconductor vignettes", {
-  skip_on_cran()
   skip_if_not_installed("MassSpecWavelet")
-  skip_if_not_installed("BiocManager")
 
-  local_options(
-    "repos" = c("CRAN" = "https://cran.rstudio.com")
-  )
+  # local_options(
+  #   "repos" = c("CRAN" = "https://cran.rstudio.com")
+  #)
 
   expect_equal(
     href_expr_(vignette("MassSpecWavelet", "MassSpecWavelet")),
@@ -179,15 +177,10 @@ test_that("can link to bioconductor vignettes", {
 test_that("can link to remote articles", {
   skip_on_cran()
 
-  local_options(
-    "repos" = c("CRAN" = "https://cran.rstudio.com")
-  )
-
   expect_equal(
     href_expr_(vignette("sha1", "digest")),
     "https://cran.rstudio.com/web/packages/digest/vignettes/sha1.html"
   )
-
   expect_equal(href_expr_(vignette("blah1", "digest")), NA_character_)
 
   expect_equal(
@@ -207,11 +200,7 @@ test_that("or local sites, if registered", {
 })
 
 test_that("looks in attached packages", {
-  skip_on_cran()
-  local_options(
-    "downlit.attached" = c("grid", "digest"),
-    "repos" = c("CRAN" = "https://cran.rstudio.com")
-  )
+  local_options("downlit.attached" = c("grid", "digest"))
 
   expect_equal(
     href_expr_(vignette("sha1")),
