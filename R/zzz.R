7dd994ef2fafe9fd7801ec28c10413b161b21382
Hadley Wickham <h.wickham@gmail.com>
2022-07-04 19:38:15 -0500

    Silence R CMD check note

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,7 @@
 .onLoad <- function(libname, pkgname) {
   repo_urls <<- memoise::memoise(repo_urls)
   CRAN_urls <<- memoise::memoise(CRAN_urls)
+
+  # Silence R CMD check note, since only used in memoised function
+  withr::local_envvar
 }
