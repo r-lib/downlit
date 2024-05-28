0c3455c122ae683c317ab4f8cada4c7debbe95ed
Hadley Wickham <h.wickham@gmail.com>
2024-05-28 12:39:52 -0500

    Simpler parsing algorthim
    Fixes #189

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -6,3 +6,7 @@ test_that("converts Latin1 encoded text to utf8", {
   expect_equal(Encoding(y), "UTF-8")
   expect_equal(y, "\u00fc")
 })
+
+test_that("doesn't crash on utf-8 characters", {
+  expect_equal(safe_parse("×"), NULL)
+})
